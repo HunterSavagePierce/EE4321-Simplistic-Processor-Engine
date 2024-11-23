a9bd0bdf557d36933f28a80e01980e3ba515ba75
Hunter Savage-Pierce <huntersavagepierce@gmail.com>
2024-11-22 19:36:38 -0600

    Initial commit

---
+++ Final_Project.srcs/sources_1/imports/Final Project/testBench.sv
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////
+// Project: Execution Engine
+// Author: Hunter Savage-Pierce
+// Date: November 18th, 2024
+// Version: 1.0
+///////////////////////////////////////////////////////////////////////////////
+// Description:
+// Test Bench File for a Custom Execution Engine
+//
+// References:
+// - Mark W. Welker EE4321 Execution Engine Supplied Code Texas State University
+// - ChatGPT 4o
+///////////////////////////////////////////////////////////////////////////////
+
+
+module TestMatrix  (Clk,nReset);
+
+	output logic Clk,nReset; // we are driving these signals from here. 
+
+	initial begin
+		Clk = 0;
+		nReset = 1;
+	#5 nReset = 0;
+	#5 nReset = 1;
+	end
+	
+	always  #5 Clk = ~Clk;
+
+	
+	endmodule
