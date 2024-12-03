41e21bd722c1cfcf3b2953b2657a5204bd9043c2
Hunter Savage-Pierce <huntersavagepierce@gmail.com>
2024-12-03 13:08:59 -0600

    Updated with provided files in class

--- Final_Project.srcs/sources_1/imports/Final Project/Instruct.sv
+++ Final_Project.srcs/sources_1/imports/Final Project/Instruct.sv
@@ -1,22 +1,13 @@
-///////////////////////////////////////////////////////////////////////////////
-// Project: Simplistic Processing Engine
-// Author: Hunter Savage-Pierce
-// Date: November 22nd, 2024
-// Version: 1.0
-///////////////////////////////////////////////////////////////////////////////
-// Description:
-// Instruction Memory for a Simplistic Processing Engine
+// Mark W. welker
+// Instruction memory. 
+// holds the instructions that the processor will execute.
 //
-// References:
-// - Mark W. Welker EE4321 Simplistic Processing Engine Supplied Code Texas State University
-// - ChatGPT 4o
-///////////////////////////////////////////////////////////////////////////////
-
 // the address lines are generic and each module must handle thier own decode. 
 // The address bus is large enough that each module can contain a local address decode. This will save on multiple enmables. 
 // bit 11-0 are for adressing inside each unit.
 // nWrite = 0 means databus is being written into the part on the falling edge of write
 // nRead = 0 means it is expected to drive the databus while this signal is low and the address is correct until the nRead goes high independent of addressd bus.
+
 
 parameter MainMemEn = 0;
 parameter RegisterEn = 1;
@@ -53,68 +44,71 @@ parameter Overflow_err = 5;
 
 // add the data at location 0 to the data at location 1 and place result in location 2
 parameter Instruct1 = 32'h 03_02_00_01; // add first matrix to second matrix store in memory
-parameter Instruct2 = 32'h 10_10_0a_0b; // add 16 bit numbers in location a to b store in temp register
-parameter Instruct3 = 32'h 04_03_02_00; //Subtract the first matrix from the result in step 1 and store the result somewhere else in memory. 
-parameter Instruct4 = 32'h 05_04_02_00;//Transpose the result from step 1 store in memory
-parameter Instruct5 = 32'h 07_11_03_10;//ScaleImm the result in step 3 by the result from step 2 store in a matrix register
-parameter Instruct6 = 32'h 00_05_04_03; //Multiply the result from step 4 by the result in step 3, store in memory. 4x4 * 4x4
-parameter Instruct7 = 32'h 01_06_04_03; //Multiply the result from step 6 by the result in step 5, store in memory. 4x2 * 2x4
-parameter Instruct8 = 32'h 02_07_04_03; //Multiply the result from step 5 by the result in step 4, store in memory. 2x4 * 4x2
-parameter Instruct9 = 32'h 12_0a_01_00;//Multiply the integer value in memory location 0 to location 1. Store it in memory location 0x0A
-parameter Instruct10 = 32'h 11_12_0a_01;//Subtract the integer value in memory location 01 from memory location 0x0A and store it in a register
-parameter Instruct11 = 32'h 13_0b_0a_12;//Divide Memory location 0x0A by the register in step 8 and store it in location 0x0B
-parameter Instruct12 = 32'h FF_00_00_00; // stop
+parameter Instruct2 = 32'h 06_03_00_0a; // scale matrix 1 by whats in location A store in memory
+parameter Instruct3 = 32'h 10_10_0a_0b; // add 16 bit numbers in location a to b store in temp register
+parameter Instruct4 = 32'h 04_04_03_00; //Subtract the first matrix from the result in step 2 and store the result somewhere else in memory. 
+parameter Instruct5 = 32'h 05_05_02_00;//Transpose the result from step 1 store in memory
+parameter Instruct6 = 32'h 07_11_03_08;//ScaleImm the result in step 2 by the result from step 3 store in a matrix register
+parameter Instruct7 = 32'h 00_06_04_05; //Multiply the result from step 4 by the result in step 5, store in memory. 4x4 * 4x4
+parameter Instruct8 = 32'h 01_07_11_05; //Multiply the result from step 6 by the result in step 5, store in memory. 4x2 * 2x4
+parameter Instruct9 = 32'h 02_08_05_04; //Multiply the result from step 5 by the result in step 4, store in memory. 2x4 * 4x2
+
+parameter Instruct10 = 32'h 12_0a_01_00;//Multiply the integer value in memory location 0 to location 1. Store it in memory location 0x0A
+parameter Instruct11 = 32'h 11_12_0a_01;//Subtract the integer value in memory location 01 from memory location 0x0A and store it in a register
+parameter Instruct12 = 32'h 13_0b_07_08;//Divide the result from step 8 by the result in step 9  and store it in location 0x0B
+parameter Instruct13 = 32'h FF_00_00_00; // stop
 
 
-module InstructionMemory(Clk, Databus, address, nRead,nReset);
+module InstructionMemory(Clk,Dataout, address, nRead,nReset);
 // NOTE the lack of datain and write. This is because this is a ROM model
-    
-    input logic nRead, nReset, Clk;
-    input logic [15:0] address;
-    
-    output logic [31:0] Databus; // 1 - 32 it instructions at a time.
 
-    logic [31:0]InstructMemory[12]; // this is the physical memory
-    
-    logic [255:0] Databus_driver; // Internal driver for Databus
-    logic drive_enable;
+input logic nRead, nReset, Clk;
+input logic [15:0] address;
 
-    // This memory is designed to be driven into a data multiplexor. 
-    
-    assign Databus = drive_enable ? Databus_driver : 'z;
+inout logic [31:0] Dataout; // 1 - 32 it instructions at a time.
 
-    always_ff @(negedge Clk or negedge nReset) begin
-        if (!nReset) begin
-            Databus_driver = 0;
-            drive_enable <= 0;
-        end else begin
-            if (address[15:12] == InstrMemEn) begin // talking to Instruction IntstrMemEn
-                if (~nRead) begin
-                    drive_enable <= 1;
-                    Databus_driver <= InstructMemory[address[11:0]]; // data will reamin on dataout until it is changed.
-                end
-            end else begin
-                drive_enable <= 0;
-            end
-        end
-    end // from negedge nRead	
+  logic [31:0]InstructMemory[13]; // this is the physical memory
+  logic ItsMe; // the address bus is talkig to this module. used to enable tristate buffers.
+  logic [31:0] InstToOutput; // this is a temporary data register to be set to go to the output. 
+// This memory is designed to be driven into a data multiplexor. 
 
-    always @(negedge nReset) begin
-        //	set in the default instructions 
-        InstructMemory[0] = Instruct1;  	
-        InstructMemory[1] = Instruct2;  	
-        InstructMemory[2] = Instruct3;
-        InstructMemory[3] = Instruct4;	
-        InstructMemory[4] = Instruct5;
-        InstructMemory[5] = Instruct6;
-        InstructMemory[6] = Instruct7;
-        InstructMemory[7] = Instruct8;
-        InstructMemory[8] = Instruct9;
-        InstructMemory[9] = Instruct10;
-        InstructMemory[10] = Instruct11;
-        InstructMemory[11] = Instruct12;
-    end 
+  always_ff @(negedge Clk or negedge nReset)
+begin
+  if (!nReset)
+    InstToOutput = 0;
+  else begin
+  if(address[15:12] == InstrMemEn) // talking to Instruction IntstrMemEn
+		begin
+		    ItsMe = 1;
+			if(~nRead)begin
+				InstToOutput <= InstructMemory[address[11:0]]; // data will reamin on dataout until it is changed.
+			end
+		end
+	else ItsMe = 0; 
+	end
+end // from negedge nRead	
+
+always @(negedge nReset)
+begin
+//	set in the default instructions 
+//
+	InstructMemory[0] = Instruct1;  	
+	InstructMemory[1] = Instruct2;  	
+  	InstructMemory[2] = Instruct3;
+	InstructMemory[3] = Instruct4;	
+	InstructMemory[4] = Instruct5;
+	InstructMemory[5] = Instruct6;
+	InstructMemory[6] = Instruct7;
+	InstructMemory[7] = Instruct8;
+	InstructMemory[8] = Instruct9;
+	InstructMemory[9] = Instruct10;
+	InstructMemory[10] = Instruct11;
+	InstructMemory[11] = Instruct12;
+	InstructMemory[12] = Instruct13;
+end 
+
+assign Dataout = ItsMe ? InstToOutput : 32'bz;
+
+
 
 endmodule
-
-
--- Final_Project.srcs/sources_1/imports/Final Project/MainMem.sv
+++ Final_Project.srcs/sources_1/imports/Final Project/MainMem.sv
@@ -1,63 +1,62 @@
-///////////////////////////////////////////////////////////////////////////////
-// Project: Simplistic Processing Engine
-// Author: Hunter Savage-Pierce
-// Date: November 22nd, 2024
-// Version: 1.0
-///////////////////////////////////////////////////////////////////////////////
-// Description:
-// Main Memory File for a Simplistic Processing Engine
+// Mark W. Welker
+// project
+// Spring 2023
 //
-// References:
-// - Mark W. Welker EE4321 Simplistic Processing Engine Supplied Code Texas State University
-// - ChatGPT 4o
-///////////////////////////////////////////////////////////////////////////////
+//
 
-module MainMemory(Clk, Databus, address, nRead,nWrite, nReset);
-    input logic nRead,nWrite, nReset, Clk;
-    input logic [15:0] address;
-    inout logic [255:0] Databus;
 
-    logic [255:0]MainMemory[14]; // this is the physical memory
-    
-    logic [255:0] Databus_driver; // Internal driver for Databus
-    logic drive_enable;
-  
-    // Tri-state control for the Databus
-    assign Databus = drive_enable ? Databus_driver : 'z;
-    
-    always_ff @(negedge Clk or negedge nReset) begin
-        if (~nReset) begin
-            drive_enable = 0;
-            Databus_driver = 0;
-            MainMemory[0] = 256'h0008_000c_0008_0006_000c_0010_000d_0009_000a_0009_0005_000d_000c_0003_000a_0006;
-            MainMemory[1] = 256'h0003_0004_0007_0008_0007_0008_000e_0007_0010_0009_000c_000b_000c_0005_0005_0006;
-            MainMemory[2] = 256'h0;
-            MainMemory[3] = 256'h0;
-            MainMemory[4] = 256'h0;
-            MainMemory[5] = 256'h0;
-            MainMemory[6] = 256'h0;
-            MainMemory[7] = 256'h0;
-            MainMemory[8] = 256'h0;
-            MainMemory[9] = 256'h0;
-            MainMemory[10] = 256'h7;
-            MainMemory[11] = 256'hb;
-            MainMemory[12] = 256'h0;
-            MainMemory[13] = 256'h0;
-        end else if (address[15:12] == MainMemEn) begin
-            if (~nRead) begin
-                drive_enable = 1;
-                Databus_driver = MainMemory[address[11:0]];
-            end else begin
-                drive_enable = 0;
-            end
-    
-            if (~nWrite) begin
-                MainMemory[address[11:0]] <= Databus; // Use `Databus` for input
-            end
-        end else begin
-            drive_enable = 0; // Ensure bus is not driven when not active
-        end
-    end	
+
+module MainMemory(Clk,Dataout, address, nRead,nWrite, nReset);
+
+
+
+input logic nRead,nWrite, nReset, Clk;
+input logic [15:0] address;
+
+inout logic [255:0] Dataout; // to the CPU 
+
+  logic [255:0]MainMemory[14]; // this is the physical memory
+  logic ItsMe; // the address bus is talkig to this module. used to enable tristate buffers.
+  logic [255:0] MemToOutput; // this is a temporary data register to be set to go to the output. 
+
+always_ff @(negedge Clk or negedge nReset)
+begin
+	if (~nReset) begin
+	MainMemory[0] = 256'h0009_000c_0008_0007_000c_0010_000d_0009_000B_0009_0006_000d_000d_0005_000e_0006;
+	MainMemory[1] = 256'h0007_0005_0011_0009_000c_0008_000e_0007_0010_0009_000c_000b_000c_0007_0009_0006;
+	MainMemory[2] = 256'h0;
+	MainMemory[3] = 256'h0;
+	MainMemory[4] = 256'h0;
+	MainMemory[5] = 256'h0;
+	MainMemory[6] = 256'h0;
+	MainMemory[7] = 256'h0;
+	MainMemory[8] = 256'h0;
+	MainMemory[9] = 256'h0;
+	MainMemory[10] = 256'h9;
+	MainMemory[11] = 256'ha;
+	MainMemory[12] = 256'h0;
+	MainMemory[13] = 256'h0;
+	
+	
+      MemToOutput=0;
+	end
+
+  else if(address[15:12] == MainMemEn) // talking to Instruction
+		begin
+			
+			if (~nRead)begin
+			ItsMe <= 1; // Only Drive Bus on read
+				MemToOutput <= MainMemory[address[11:0]]; // data will remain on dataout until it is changed.
+			end
+			if(~nWrite)begin
+			ItsMe <= 0; // only drive bus on read
+		    MainMemory[address[11:0]] <= Dataout;
+			end
+		end
+    else ItsMe = 0;
+end 	
+
+assign Dataout = ItsMe ? MemToOutput : 256'bz;
 endmodule
 
 
